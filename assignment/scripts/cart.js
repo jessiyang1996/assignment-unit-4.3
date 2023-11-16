659cb42580b55c3dac2d735f2833b1510b3a8af4
jessiyang1996 <jessiyang1996@gmail.com>
2023-11-16 16:58:58 -0600

    after class

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,56 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// Create a global variable named basket and set it to an empty array.
+let basket = [];
 
+// Create a function called addItem. It should:
+    //take an input parameter for a string item
+    // add the new item to the global array basket.
+    //return true indicating the item was added
+
+function addItem( item ){
+    console.log( 'in addItem Function');
+    basket.push( item );
+    return true;
+};
+
+console.log( '===!!! addItem Function Test !!!===');
+item = 'eggs';
+console.log( addItem(item) );
+console.log( basket ); // to see if it was actually added to basket and that our function works.
+// our function works, eggs appear in basket in console.
+
+// using our function to add items to our cart so that we can test for the next question.
+addItem ( 'apple');
+addItem ( 'cheese');
+addItem ( 'pumpkin pie');
+console.log( basket ); 
+
+// Create a function called listItems. It should:
+    // loop over the items in the basket array
+    // console.log each individual item on a new line
+    
+function listItems(){
+    console.log( 'in listItems Function')
+    for(i=0; i<basket.length; i++){
+        console.log( basket[i] )
+    }
+}
+
+console.log(listItems()); // test - it works on console. Each item appears on an
+//individual line.
+
+// Create a function called empty. It should:
+    // reset the basket to an empty array
+
+function empty(){
+    console.log( 'in empty Function');
+        basket = [];
+}
+
+empty();
+console.log( basket );
 
 
 
